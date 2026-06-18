40d4681affa17964425eaef346a4eccc944bd7d2
Yuta <63215922+shim517@users.noreply.github.com>
2026-06-18 11:40:11 +0700

    Update main.tf

--- terraform/services/redemption/rds/main.tf
+++ terraform/services/redemption/rds/main.tf
@@ -163,3 +163,27 @@ resource "aws_appautoscaling_policy" "rds_cpu" {
     scale_out_cooldown = 120
   }
 }
+
+
+resource "aws_db_proxy" "example" {
+  name                   = "example"
+  debug_logging          = false
+  engine_family          = "MYSQL"
+  idle_client_timeout    = 1800
+  require_tls            = true
+  role_arn               = aws_iam_role.example.arn
+  vpc_security_group_ids = [aws_security_group.example.id]
+  vpc_subnet_ids         = [aws_subnet.example.id]
+
+  auth {
+    auth_scheme = "SECRETS"
+    description = "example"
+    iam_auth    = "DISABLED"
+    secret_arn  = aws_secretsmanager_secret.example.arn
+  }
+
+  tags = {
+    Name = "example"
+    Key  = "value"
+  }
+}
